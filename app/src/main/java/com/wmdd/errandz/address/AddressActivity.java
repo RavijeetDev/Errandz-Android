96c0838e4b0241cf6b3772a26e617304b69a8ee0
RavijeetDev <rahluwalia01@mylangara.bc.ca>
2020-07-23 20:54:09 -0700

    Add Google Map API

--- app/src/main/java/com/wmdd/errandz/address/AddressActivity.java
+++ app/src/main/java/com/wmdd/errandz/address/AddressActivity.java
@@ -1,16 +1,129 @@
 package com.wmdd.errandz.address;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.maps.GoogleMap;
+import com.google.android.libraries.maps.OnMapReadyCallback;
+import com.google.android.libraries.maps.SupportMapFragment;
+import com.google.android.libraries.maps.model.LatLng;
+import com.google.android.libraries.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.wmdd.errandz.R;
+import com.wmdd.errandz.userProfileEdit.UserProfileEditActivity;
 
-public class AddressActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class AddressActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final int AUTOCOMPLETE_REQUEST_CODE = 100;
+    private com.wmdd.errandz.bean.Address address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        findViewById(R.id.select_address).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSetAddressCalled();
+            }
+        });
+    }
+
+    public void onSetAddressCalled() {
+        // Set the fields to specify which types of place data to return.
+        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG);
+        // Start the autocomplete intent.
+        Intent intent = new Autocomplete.IntentBuilder(
+                AutocompleteActivityMode.FULLSCREEN, fields).setCountry("CA")
+                .build(this);
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(49.1402515, -122.8369358))
+                .title("Marker"));
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                String fullAddress = place.getAddress();
+//                addressTextInputLayout.getEditText().setText(fullAddress);
+
+                try {
+                    List<Address> addresses;
+                    Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+                    try {
+                        addresses = geocoder.getFromLocation(place.getLatLng().latitude, place.getLatLng().longitude, 1);
+                        String house = addresses.get(0).getSubThoroughfare();
+                        String street = addresses.get(0).getThoroughfare();
+                        String city = addresses.get(0).getLocality();
+                        String state = addresses.get(0).getAdminArea();
+                        String country = addresses.get(0).getCountryName();
+                        String postalCode = addresses.get(0).getPostalCode();
+                        Log.e("House No: ", "" + house);
+                        Log.e("Street: ", "" + street);
+                        Log.e("AddressCity: ", "" + city);
+                        Log.e("AddressState: ", "" + state);
+                        Log.e("AddressCountry: ", "" + country);
+                        Log.e("AddressPostal: ", "" + postalCode);
+                        Log.e("AddressLatitude: ", "" + place.getLatLng().latitude);
+                        Log.e("AddressLongitude: ", "" + place.getLatLng().longitude);
+
+                        address = new com.wmdd.errandz.bean.Address();
+                        address.setFullAddress(fullAddress);
+                        address.setStreetAddress("" + house + " " + street);
+                        address.setCity(city);
+                        address.setProvince(state);
+                        address.setPostalCode(postalCode);
+                        address.setCountry(country);
+                        address.setLatitude(String.valueOf(place.getLatLng().latitude));
+                        address.setLongitude(String.valueOf(place.getLatLng().longitude));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    //setMarker(latLng);
+                }
+
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                // TODO: Handle the error.
+                Status status = Autocomplete.getStatusFromIntent(data);
+                Toast.makeText(AddressActivity.this, "Error: " + status.getStatusMessage(), Toast.LENGTH_LONG).show();
+                Log.i("UserProfileEditActivity", status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
     }
 }
